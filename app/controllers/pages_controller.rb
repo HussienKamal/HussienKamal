ec8b57fa0f84e56db72febf4d196da7d30506970
Hussien Kamal <hussienelhady14@gmail.com>
2015-02-26 19:25:30 +0200

    APP

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def profiles
+  end
+  
+  def hussien
+  end
+  
+  def nageeb
+  end
+  
+  def omar
+  end
+  
+  def monica
+  end
+  
+  def bergitte
+  end
+  
+  def jirayr
+  end
+  
+  def mariam
+  end
+  
+  def contact
+  end
+  
+  def blog
+  end
+end
